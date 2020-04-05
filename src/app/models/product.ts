8c1ad5e48269483319cd24dcfda04cb53dffc929
Akathian <akathian05@gmail.com>
2020-04-05 17:50:11 -0400

    change placeholder pics, product class

---
+++ src/app/models/product.ts
@@ -0,0 +1,17 @@
+import { ThrowStmt } from '@angular/compiler';
+
+export class Product {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+
+    constructor(id: number, name: string, description: string, price: number, imageUrl: string) {
+        this.id = id
+        this.name = name
+        this.description = description
+        this.price = price
+        this.imageUrl = imageUrl
+    }
+}
