21e8352e056a838658999e4170df9249099b59dc
Akathian <akathian05@gmail.com>
2020-04-06 13:14:02 -0400

    functional cart

--- src/app/components/shopping-cart/cart/cart.component.ts
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MessengerService } from 'src/app/services/messenger.service'
+import { Product } from 'src/app/models/product';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  cartItems = [
+    // { id: 1, productId: 1, productName: 'Product 1', qty: 2, price: 39 },
+    // { id: 2, productId: 3, productName: 'Product 3', qty: 1, price: 59 }
+  ]
+
+  cartTotal = 0
+
+  constructor(private msg: MessengerService) { }
 
   ngOnInit() {
+    this.msg.getMsg().subscribe((product: Product) => {
+      this.addProductToCart(product)
+    })
   }
+
+  addProductToCart(product: Product) {
+    const newItem = {
+      productId: product.id,
+      productName: product.name,
+      qty: 1,
+      price: product.price
+    }
+
+    let productExists = false
+
+    for (let i = 0; i < this.cartItems.length; i += 1) {
+      const item = this.cartItems[i]
+      if (item.productId === product.id) {
+        productExists = true
+        item.qty += 1
+        break;
+      }
+    }
+    if (!productExists) {
+      this.cartItems.push(newItem)
+    }
+
+    this.cartTotal = 0
+    this.cartItems.forEach(item => {
+      this.cartTotal += item.price * item.qty
+    })
+  }
+
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Product } from 'src/app/models/product'
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
-  constructor() {
 
+export class ProductService {
+
+  products: Product[] = [
+    new Product(1, 'Product1', 'This is product 1 description', 39, 'https://images.saatchiart.com/saatchi/976708/art/5789653/4859453-DPDBIBXN-7.jpg'),
+    new Product(2, 'Product2', 'This is product 2 description', 29, 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/poppyscape-sunset-impasto-palette-knife-acrylic-painting-mona-edulesco-mona-edulesco.jpg'),
+    new Product(3, 'Product3', 'This is product 3 description', 59, 'https://i0.wp.com/stepbysteppainting.net/wp-content/uploads/2019/01/img_3259-e1546635144542.jpg?resize=619%2C800&ssl=1'),
+    new Product(4, 'Product4', 'This is product 4 description', 39, 'https://media.overstockart.com/optimized/cache/data/product_images/MU925-1000x1000.jpg'),
+    new Product(5, 'Product5', 'This is product 5 description', 39, 'https://cdn.evbuc.com/eventlogos/92542003/bonsainew.jpg'),
+    new Product(6, 'Product6', 'This is product 6 description', 39, 'https://images.unsplash.com/photo-1541512416146-3cf58d6b27cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'),
+    new Product(7, 'Product7', 'This is product 7 description', 39, 'https://s3fs.paintnite.com/yaymaker-images/nite-out/original/8786-andromeda-flower.jpg')
+  ]
+
+  constructor() {
+  }
+
+  getProducts(): Product[] {
+    return this.products
   }
 }
