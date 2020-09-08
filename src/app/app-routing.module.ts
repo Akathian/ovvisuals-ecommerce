bedeea6abd7c9d8682c70204ade15d13d77f7b5d
Akathian <akathian05@gmail.com>
2020-09-07 23:24:40 -0400

    deleted useless components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { BrowserModule } from '@angular/platform-browser';
 
-import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
-import { CustomComponent } from './components/shopping-cart/product-list/custom/custom.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CustomComponent } from './components/custom/custom.component';
 import { AboutComponent } from './components/about/about.component';
-import { ProductPageComponent } from './components/shopping-cart/product-page/product-page.component';
+import { ProductPageComponent } from './components/product-list/product-page/product-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
