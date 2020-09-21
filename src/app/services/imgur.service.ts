143834579abfb31b6e36cf3a9fa52badb8693954
Akathian <akathian05@gmail.com>
2020-09-21 00:11:36 -0400

    smtpjs email + imgur

---
+++ src/app/services/imgur.service.ts
@@ -0,0 +1,112 @@
+import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment"
+import { ImageTools } from "../../assets/js/imgTools"
+@Injectable({
+  providedIn: 'root'
+})
+export class ImgurService {
+  access = "aa";
+  albumName;
+  files;
+  uploadedImgs = [];
+  imgsHTML = ''
+  constructor() { }
+
+  async auth(files) {
+    let formdata = new FormData();
+    formdata.append("refresh_token", environment.imgur.refresh);
+    formdata.append("client_id", environment.imgur.client);
+    formdata.append("client_secret", environment.imgur.secret);
+    formdata.append("grant_type", "refresh_token");
+
+    let requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+    const access = JSON.parse(await (await fetch("https://api.imgur.com/oauth2/token", <RequestInit>requestOptions)).text()).access_token
+    await this.createAlbum(access, files)
+  }
+
+  async createAlbum(access, files) {
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${access}`);
+    // myHeaders.append("Authorization", `Client-ID ${environment.imgur.client}`);
+    let name = "testAlbum"
+    var formdata = new FormData();
+    // formdata.append("ids[]", files);
+    formdata.append("title", name);
+    formdata.append("description", `Album ${name}`);
+    formdata.append("privacy", "hidden");
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    // const albumId = JSON.parse(await (await fetch("https://api.imgur.com/3/album", <RequestInit>requestOptions)).text()).data.id
+    const albumId = "d40tRSN"
+    for (let i = 0; i < files.length; i++) {
+      await this.createImg(access, albumId, files[i])
+    }
+  }
+
+  async createImg(access, albumid, file) {
+    let self = this
+    let resizeFile: File;
+    ImageTools.resize(file, {
+      width: 320, // maximum width
+      height: 240 // maximum height
+    }, async function (blob, didItResize) {
+      // didItResize will be true if it managed to resize it, otherwise false (and will return the original file as 'blob')
+      var b: any = blob
+      b.lastModifiedDate = new Date();
+      resizeFile = new File([blob], file.name, { type: "image/jpeg", lastModified: Date.now() });
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${access}`);
+
+      var formdata = new FormData();
+      formdata.append("image", resizeFile);
+      formdata.append("album", albumid);
+
+
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: formdata,
+        redirect: 'follow'
+      };
+      const imageURL = JSON.parse(await (await fetch("https://api.imgur.com/3/image", <RequestInit>requestOptions)).text()).data.link
+      const imgTag = `<img alt="" src="${imageURL}" width="320px" class="mcnImage">`
+      const img = `                                
+  <table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnImageCardBlock">
+  <tbody class="mcnImageCardBlockOuter">
+    <tr>
+        <td class="mcnImageCardBlockInner" valign="top"
+            style="padding-top:9px; padding-right:18px; padding-bottom:9px; padding-left:18px;">
+  
+            <table align="right" border="0" cellpadding="0" cellspacing="0"
+                class="mcnImageCardBottomContent" width="100%"
+                style="background-color: #404040;">
+                <tbody>
+                    <tr>
+                        <td class="mcnImageCardBottomImageContent" align="center" valign="top" style="padding-top:0px; padding-right:0px; padding-bottom:0; padding-left:0px;">
+                            ${imgTag}
+                        </td>
+                    </tr>
+                   
+                </tbody>
+            </table>
+        </td>
+    </tr>
+  </tbody>
+  </table>`
+
+      self.uploadedImgs.push(imageURL)
+      self.imgsHTML += img
+    });
+
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,6 +16,11 @@ export const environment = {
   paypal: {
     client: "AUzHPk1_nr1WKmm9NE6srTNO1TWEfggxW04RZELfVYpxw5qCBV0Y2rXawKfNwZa434URXOcwy6XFbms5",
     secret: "ELYUuMU1e5MugtOxhwmVLNwbc-_ZyP6KwEQ0dMzyMi45qjColyVBDz9h94S3WRbRzBA1Y9CYc_tKIKU_"
+  },
+  imgur: {
+    refresh: "47e328a6eb81a9247976c9d93b01f2a449199491",
+    client: "40a4c07be6fae27",
+    secret: "5d1e167cd36ad2ac1fde7820253e51fc80be00b9"
   }
 };
 
